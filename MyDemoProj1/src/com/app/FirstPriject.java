db7ef89bf531ed42191d34ad75d4f86494479587
shri <shrigithub@gmail.com>
2020-04-09 05:54:53 -0700

    adding main methode with FirstPriject class

---
+++ MyDemoProj1/src/com/app/FirstPriject.java
@@ -0,0 +1,12 @@
+package com.app;
+
+public class FirstPriject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git Hub ...");
+
+	}
+
+}
